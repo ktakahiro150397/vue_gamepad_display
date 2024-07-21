6f034bb150dd2d1c280bd1c94db9098ada99ec77
ktakahiro150397 <k_takahiro_150397@yahoo.co.jp>
2024-07-21 11:31:06 +0900

    ゲームループ省エネ部分修正

--- src/components/KeyInputElement.vue
+++ src/components/KeyInputElement.vue
@@ -69,11 +69,10 @@ export default defineComponent({
   mounted() {
     if (this.isFreeze || !this.isDisplayFrameCount) {
       // フリーズ状態またはフレームカウント非表示の場合、ゲームループを実行しない
-    }
       return;
     } else {
-    const gameLoop = GameLoop.instance;
-    gameLoop.executeGameLoop(this.onGameLoop);
+      const gameLoop = GameLoop.instance;
+      gameLoop.executeGameLoop(this.onGameLoop);
     }
   },
 });
