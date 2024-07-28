b84eb5446de24fe53df177983c29257b8cd37e0d
ktakahiro150397 <k_takahiro_150397@yahoo.co.jp>
2024-07-28 21:40:15 +0900

    electronビルド設定追加

--- src/background.ts
+++ src/background.ts
@@ -13,8 +13,9 @@ protocol.registerSchemesAsPrivileged([
 async function createWindow() {
   // Create the browser window.
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    title: "キー入力表示アプリ",
+    frame: true,
+    autoHideMenuBar: true,
     webPreferences: {
 
       // Use pluginOptions.nodeIntegration, leave this alone
